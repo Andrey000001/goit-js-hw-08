de107450d3ab93bf5d33e46e0aa8bd74b98b3651
Андрей <kovratovskiy1999@gmail.com>
2023-12-29 08:58:43 -0500

    Begen

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,5 @@
-
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
 import { galleryItems } from './gallery-items';
-import colorCardTpl from '../templates/color-card.hbs';
+// Change code below this line
 
-
-const galleryEl = document.querySelector('.gallery');
-const galleryItemsEl = createGallery(galleryItems);
-
-function createGallery(galleryItemsEl) {
-  return colorCardTpl(galleryItemsEl);
-}
-
-galleryEl.addEventListener('click', onClick);
-galleryEl.innerHTML = galleryItemsEl;
-function onClick(evt) {
-  evt.preventDefault();
-  if (evt.target.nodeName !== 'IMG') {
-    return;
-  }
-  lightbox.open()
-}
-
-var lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-});
+console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,1 @@
-import Player from '@vimeo/player';
-const CURRENT_TIME = 'videoplayer-current-time';
-const vimeoPlayer = document.querySelector('#vimeo-player');
 
-const player = new Player(vimeoPlayer);
-
-player.on('timeupdate', onPlay, 1000);
-
-function onPlay({ seconds }) {
-  localStorage.setItem(CURRENT_TIME, seconds);
-}
-
-setCurrentTime();
-function setCurrentTime() {
-  if (!localStorage.getItem(CURRENT_TIME)) {
-    return;
-  }
-  player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
-}
