50c59ebf852e7b4c77b6cd48c02643dd22c30d1e
Андрей <kovratovskiy1999@gmail.com>
2023-12-29 09:51:04 -0500

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+createELGallery(galleryItems)
+
+function createELGallery(arr) {
+  const markup = arr.map(({preview, original, description }) => 
+      `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>`).join('')
+  gallery.innerHTML = markup;
+}
+gallery.addEventListener('click',(e) => {
+  e.preventDefault()
+  
+  if(e.target !== 'IMG') {
+    return
+  }
+  lightbox.open()
+})
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
